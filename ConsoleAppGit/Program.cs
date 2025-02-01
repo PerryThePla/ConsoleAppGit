953724c0ffe868a8795a5ef770089c7de26eff4e
giacomo.strambi <giacomo.strambi@PC2223BIS>
2025-02-01 08:51:27 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleAppGit/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleAppGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //gino gino gino gino
+        }
+    }
+}
